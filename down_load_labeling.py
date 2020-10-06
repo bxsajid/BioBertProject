769f570a41e265f61ed0689da3a9fa67cf0898e1
hamza1886 <hamza1886@gmail.com>
2020-10-06 09:00:05 +0500

    Added logic to download files which are not downloaded already, and to keep record of skipped files

--- down_load_labeling.py
+++ down_load_labeling.py
@@ -1,26 +1,42 @@
-from urllib.request import urlretrieve
-from zipfile import ZipFile
+import io
+import zipfile
 
 import pandas as pd
+import requests
 
 data = pd.read_csv('NASID_clean_data.csv')
-SET_ID = data['SET ID']
 
-url1 = 'https://dailymed.nlm.nih.gov/dailymed/getFile.cfm?setid='
-url2 = '&type=zip'
-outdir = 'XML Files'
+out_dir = 'XML Files/'
+last_set_id = open('temp_file.txt', 'r').read()
+searching_set_id = True
 
-for row in SET_ID:
-    sky = row
-    link = url1 + sky + url2
-    name = outdir + sky + '.zip'
-    urlretrieve(link, name)
-    with ZipFile(name, 'r') as zipObj:
-        # Get a list of all archived file names from the zip
-        listOfFileNames = zipObj.namelist()
-        # Iterate over the file names
-        for fileName in listOfFileNames:
-            # Check filename endswith csv
-            if fileName.endswith('.xml'):
-                # Extract a single file from zip
-                zipObj.extract(fileName, outdir)
+for i, SET_ID in enumerate(data['SET ID']):
+    # skip all SET_ID up till last_set_id
+    if last_set_id != '' and searching_set_id and SET_ID != last_set_id:
+        continue
+    searching_set_id = False
+
+    # save current SET_ID to temp file
+    open('temp_file.txt', 'w').write(SET_ID)
+
+    # download ZIP file
+    link = f'https://dailymed.nlm.nih.gov/dailymed/getFile.cfm?setid={SET_ID}&type=zip'
+
+    try:
+        r = requests.get(link)
+        print(f'fetching file [{i + 1}]: {r.url}')
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+    except Exception as e:
+        print(e)
+        open('skipped_files.txt', 'a').write(SET_ID + '\n')
+        continue
+
+    listOfFileNames = z.namelist()
+
+    for fileName in listOfFileNames:
+        # extract XML file ONLY
+        if fileName.endswith('.xml'):
+            z.extract(fileName, out_dir)
+
+# empty temp file when all XML files are downloaded
+open('temp_file.txt', 'w').write('')
