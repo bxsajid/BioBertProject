becdec25b316f8e1426f853a7c6c7135cd09daaa
hamza1886 <hamza1886@gmail.com>
2020-10-06 07:06:09 +0500

    Refactor code

--- BioBERT_NER.py
+++ BioBERT_NER.py
@@ -4,4 +4,4 @@ def query_raw(text, url="https://bern.korea.ac.kr/plain"):
 return requests.post(url, data={'sample_text': text}).json()
 
 if __name__ == '__main__':
-print(query_raw("YOUR TEXT HERE"))
+    print(query_raw("YOUR TEXT HERE"))
--- down_load_labeling.py
+++ down_load_labeling.py
@@ -1,33 +1,20 @@
-import os
-import csv
-import gzip
-import collections
-import re
-import io
-import json
-import xml.etree.ElementTree as ET
 from urllib.request import urlretrieve
 from zipfile import ZipFile
-import pickle
 
-import requests
 import pandas as pd
-import numpy as np
-from bs4 import BeautifulSoup
-
 
 data = pd.read_csv('NASID_clean_data.csv')
 SET_ID = data['SET ID']
 
 url1 = 'https://dailymed.nlm.nih.gov/dailymed/getFile.cfm?setid='
 url2 = '&type=zip'
-outdir = 'C:\\Users\\bushr\\OneDrive\\Documents\\GitHub\\BioBertProject'
+outdir = 'XML Files'
 
 for row in SET_ID:
     sky = row
-    link = url1+sky+url2
-    name = outdir+sky+'.zip'
-    urlretrieve(link,name)
+    link = url1 + sky + url2
+    name = outdir + sky + '.zip'
+    urlretrieve(link, name)
     with ZipFile(name, 'r') as zipObj:
         # Get a list of all archived file names from the zip
         listOfFileNames = zipObj.namelist()
@@ -36,4 +23,4 @@ for row in SET_ID:
             # Check filename endswith csv
             if fileName.endswith('.xml'):
                 # Extract a single file from zip
-                zipObj.extract(fileName,outdir)
+                zipObj.extract(fileName, outdir)
