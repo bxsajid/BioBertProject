cb3138b8c73b6719067510db89b37d8b45ab2cd8
hamza1886 <hamza1886@gmail.com>
2020-10-06 12:41:03 +0500

    Added feature to download labeling in JSON format

--- BioBERT_NER.py
+++ BioBERT_NER.py
@@ -1,7 +1,10 @@
 import requests
 
+
 def query_raw(text, url="https://bern.korea.ac.kr/plain"):
-return requests.post(url, data={'sample_text': text}).json()
+    return requests.post(url, data={'sample_text': text}).json()
+
 
 if __name__ == '__main__':
     print(query_raw("YOUR TEXT HERE"))
+    # print(query_raw('Corneal effects including keratitis'))
